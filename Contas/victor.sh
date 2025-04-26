a494547670b52b827ef8ce904e4a467bde2eb58b
dione566 <77499190+dione566@users.noreply.github.com>
2025-04-26 12:37:39 -0300

    Atualizar o victor.sh

--- Contas/victor.sh
+++ Contas/victor.sh
@@ -1,1 +1,1 @@
-{"5948303105":{"telegram_id":"5948303105","auth":"sfa5401a0-3013-4da9-9e38-0e4df87a2b28","user_id":"17d43d0a266","expiry":"2028-09-12 16:09:02","added_by":"ssh"}}
+{"5948303105":{"telegram_id":"5948303105","auth":"sbbe0269a-0086-4ebc-92c1-b71773689ec0","user_id":"17d43d0a266","expiry":"2028-09-12 16:09:02","added_by":"ssh"}}
