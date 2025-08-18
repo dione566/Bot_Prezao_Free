f3a5096a759b0fddd6f692a91c80ec7d59171bff
dione566 <77499190+dione566@users.noreply.github.com>
2025-08-18 06:51:52 -0400

    Update and rename botbanidos.sh to botweb2.sh

--- Bots/botbanidos.sh
+++
@@ -1,5 +0,0 @@
-python --version
-echo "BOT BANIDOS INICIADO"
-cd /sdcard/Download/Bot/Bot
-python main.py
-clear
---
+++ Bots/botweb2.sh
@@ -0,0 +1,4 @@
+python --version
+echo "INICIANDO BOT WEB..."
+cd /sdcard/Download/Bot/botweb2/
+python main.py
